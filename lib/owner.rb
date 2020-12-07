323c3f9a4f654438b6e0819fe69c2c0b2bb936c2
Michael Ernest <m.hendersun@gmail.com>
2020-12-07 22:16:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def name(name)
+    Dog.new = name
+  end
 end
